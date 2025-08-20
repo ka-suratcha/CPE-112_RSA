c80c91dfc561038d355bc89dbc38e328bdea36e8
ka-suratcha <ka.suratcha@gmail.com>
2025-08-20 17:56:23 +0700

    improve isPrime
    - add .prettierrc
    - edit isPrime func make it better

--- main.c
+++ main.c
@@ -17,8 +17,7 @@ void enterNumForDecr(int *dec, int *cLen);
 void printEResult(char msg[], int enc[]);
 void printDResult(int dec[], int cLen, int decPrev[]);
 
-int main(void)
-{
+int main(void) {
     printf("=== Simple Text Encryption Program ===\n\n");
     printf("How to use this program:\n\n");
     printf("1) Enter two prime numbers to generate the public key.\n");
@@ -40,78 +39,80 @@ int main(void)
     char msg[50];
     int primeOK = 0;
 
+    // ===== START=====
+
     // Enter p and q
     enterPQ(primeOK, &p, &q);
 
+    // caculate phi and modN
     phi = (p - 1) * (q - 1);
     modN = p * q;
     printf("n = %d\n", modN);
     printf("phi(n) = (p-1)(q-1) = %d\n", phi);
 
-    // Find possible encryption keys
+    // Find possible encryption keys and print it
+    // caculate possible encryption keys
     listEncrytKey(phi, p, q, eKeys);
-
     // Print possible encryption keys
     printEKey(eKeys);
 
     // Choose operation
     int menu;
-    while (1)
-    {
+    while (1) {
+        // print menu
         printf("\nSelect an option:\n");
         printf("1. Encrypt\n2. Decrypt\n0. Exit\n\nYour choice: ");
-        if (scanf("%d", &menu) != 1)
-        {
+        if (scanf("%d", &menu) != 1) {
             printf("Invalid input (must be an integer).\n");
             return 0;
         }
 
-        switch (menu)
-        {
-        case 1:
-        { // Encrypt
+        switch (menu) {
+        case 1: { // Encrypt
+            // enter encryption key and message
             encE = enterEncrytKey(encE, phi);
             enterMsg(msg);
 
-            for (int i = 0; i < (int)strlen(msg); ++i)
-            {
+            // encrypt message
+            for (int i = 0; i < (int)strlen(msg); ++i) {
                 enc[i] = (int)msg[i];
                 enc[i] = (int)powMod(enc[i], encE, modN);
             }
 
+            // print encryption result
             printEResult(msg, enc);
             printf("\n\nTotal characters: %d\n", (int)strlen(msg));
             break;
         }
-        case 2:
-        { // Decrypt
+        case 2: { // Decrypt
+            // enter encryption key
             encE = enterEncrytKey(encE, phi);
+
+            // find decryption key
             decD = (int)findDecrKey(encE, phi);
             printf("\nDecryption key (d) = %d\n", decD);
 
+            // enter ciphertext
             enterNumForDecr(dec, &cLen);
 
-            for (int i = 0; i < cLen; ++i)
-            {
+            // decrypt ciphertext
+            for (int i = 0; i < cLen; ++i) {
                 decPrev[i] = dec[i];
                 dec[i] = (int)powMod(dec[i], decD, modN);
             }
 
+            // print decryption result
             printDResult(dec, cLen, decPrev);
             break;
         }
-        case 0:
-            exit(0);
-        default:
-            printf("\nInvalid choice. Please enter 1, 2, or 0.\n");
+        case 0: exit(0); // Exit the program
+        default: printf("\nInvalid choice. Please enter 1, 2, or 0.\n");
         }
     }
 }
 
 /* === Core functions === */
-
-int gcd(int a, int b)
-{
+int gcd(int a, int b) {
     int g = 1;
     for (int i = 1; i <= a && i <= b; i++)
         if (a % i == 0 && b % i == 0)
@@ -119,19 +120,19 @@ int gcd(int a, int b)
     return g;
 }
 
-int isPrime(int pr)
-{
-    for (int i = 2; i <= (int)sqrt(pr); i++)
-        if (pr % i == 0)
-            return 0;
+int isPrime(int pr) {
+    if (pr < 2) return 0; // 0 and 1 are not prime numbers
+    if (pr == 2 || pr == 3) return 1; // 2 is the only even
+    if (pr % 2 == 0) return 0; // even numbers greater than 2 are not prime
+    int r = (int)sqrt((double)pr);
+    for (int i = 3; i <= r; i += 2)
+        if (pr % i == 0) return 0;
     return 1;
 }
 
-void listEncrytKey(int phi, int p, int q, int eKeys[])
-{
+void listEncrytKey(int phi, int p, int q, int eKeys[]) {
     int k = 0;
-    for (int e = 2; e < phi; e++)
-    {
+    for (int e = 2; e < phi; e++) {
         if (gcd(e, phi) == 1 && e != p && e != q)
             eKeys[k++] = e;
         if (k == 5)
@@ -139,123 +140,101 @@ void listEncrytKey(int phi, int p, int q, int eKeys[])
     }
 }
 
-void enterPQ(int ok, int *p_ptr, int *q_ptr)
-{
+void enterPQ(int ok, int *p_ptr, int *q_ptr) {
     int pTmp, qTmp;
 
-    while (true)
-    {
+    while (true) {
         printf("Enter the first prime number (p): ");
-        if (scanf("%d", &pTmp) != 1)
-        {
+        if (scanf("%d", &pTmp) != 1) {
             printf("Invalid input (must be an integer).\n");
-            while (getchar() != '\n')
-                ;
+            while (getchar() != '\n');
             continue;
         }
         ok = isPrime(pTmp);
-        if (ok == 0 || pTmp < 2)
-        {
+        if (ok == 0 || pTmp < 2) {
             printf("Invalid input (must be a prime number >= 2).\n");
         }
-        else
-            break;
+        else break;
     }
 
-    while (true)
-    {
+    while (true) {
         printf("Enter the second prime number (q): ");
-        if (scanf("%d", &qTmp) != 1)
-        {
+        if (scanf("%d", &qTmp) != 1) {
             printf("Invalid input (must be an integer).\n");
-            while (getchar() != '\n')
-                ;
+            while (getchar() != '\n');
             continue;
         }
+
         ok = isPrime(qTmp);
-        if (ok == 0 || qTmp == pTmp || qTmp < 2)
-        {
+        if (ok == 0 || qTmp == pTmp || qTmp < 2) {
             printf("Invalid input (must be a different prime >= 2).\n");
         }
-        else
-            break;
+        else break;
     }
 
     *p_ptr = pTmp;
     *q_ptr = qTmp;
 }
 
-int enterEncrytKey(int encE, int phi)
-{
-    while (1)
-    {
+int enterEncrytKey(int encE, int phi) {
+    while (1) {
         printf("Enter the encryption key (e): ");
-        if (scanf("%d", &encE) != 1)
-        {
+        if (scanf("%d", &encE) != 1) {
             printf("Invalid input (must be an integer).\n");
-            while (getchar() != '\n')
-                ;
+            while (getchar() != '\n');
             continue;
         }
+
         if (gcd(encE, phi) == 1)
             return encE; // e must be coprime with φ(n)
         printf("Invalid encryption key. Please try again.\n");
-        while ((encE = getchar()) != '\n' && encE != EOF)
-            ;
+        while ((encE = getchar()) != '\n' && encE != EOF);
     }
 }
 
-void printEKey(int eKeys[])
-{
+void printEKey(int eKeys[]) {
     printf("\nPossible encryption keys:\n");
     for (int i = 0; i < 5; i++)
         printf("%d\t", eKeys[i]);
     printf("\n");
 }
 
-void enterMsg(char *msg)
-{
+void enterMsg(char *msg) {
     printf("Enter your message (max 50 chars, no spaces — use '-' instead): ");
     scanf("%49s", msg);
 }
 
-unsigned long powMod(int base, int exp, int modN)
-{
+unsigned long powMod(int base, int exp, int modN) {
     unsigned long res;
     if (base == 0)
         return 0;
     if (exp == 0)
         return 1;
 
-    if (exp % 2 == 0)
-    {
+    if (exp % 2 == 0) {
         res = powMod(base, exp / 2, modN);
         res = (res * res) % modN;
     }
-    else
-    {
+    else {
         res = base % modN;
         res = (res * powMod(base, exp - 1, modN) % modN) % modN;
     }
     return (unsigned long)((res + modN) % modN);
 }
 
-void enterNumForDecr(int *dec, int *cLen)
-{
+void enterNumForDecr(int *dec, int *cLen) {
     int t = 0;
     printf("How many numbers in the ciphertext? ");
     scanf("%d", &t);
 
-    for (int i = 1; i <= t; ++i)
-    {
+    for (int i = 1; i <= t; ++i) {
         printf("Enter number %d: ", i);
         scanf("%d", &dec[i - 1]);
     }
     *cLen = t;
 }
 
-void printEResult(char msg[], int enc[])
-{
+void printEResult(char msg[], int enc[]) {
     printf("\nBefore encryption (characters):\n");
     for (int i = 0; i < (int)strlen(msg); ++i)
         printf("%c\t", msg[i]);
@@ -270,8 +249,7 @@ void printEResult(char msg[], int enc[])
         printf("%d\t", enc[i]);
 }
 
-void printDResult(int dec[], int cLen, int decPrev[])
-{
+void printDResult(int dec[], int cLen, int decPrev[]) {
     printf("\n\nCiphertext (numbers before decryption):\n");
     for (int i = 0; i < cLen; ++i)
         printf("%d\t", decPrev[i]);
@@ -287,11 +265,9 @@ void printDResult(int dec[], int cLen, int decPrev[])
     printf("\n");
 }
 
-unsigned int findDecrKey(int encE, int phi)
-{
+unsigned int findDecrKey(int encE, int phi) {
     int k = 1;
-    while (1)
-    {
+    while (1) {
         int r = (1 + k * phi) % encE;
         if (r == 0)
             return (unsigned int)(1 + k * phi) / encE;
